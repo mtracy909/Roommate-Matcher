de17a2810d4e4573a9ba3b13260c848071d0e801
jschaefer18 <joshschaefer@gmail.com>
2025-06-02 14:47:01 -0600

    Added login/signup html

--- flask_dem0/app/routes.py
+++ flask_dem0/app/routes.py
@@ -1,7 +1,24 @@
+from flask import render_template, redirect, url_for, flash
 from app import app
-from flask import render_template
+from app.forms import LoginForm, SignupForm
 
-@app.route('/')
-@app.route('/index')
+@app.route("/")
+@app.route("/index")
 def index():
-    return render_template('index.html')
+    return render_template("home.html", title="Home")
+
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        flash(f"Login requested for {form.username.data}")
+        return redirect(url_for("index"))
+    return render_template("login.html", title="Login", form=form)
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    form = SignupForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.username.data}")
+        return redirect(url_for("index"))
+    return render_template("signup.html", title="Sign Up", form=form)
